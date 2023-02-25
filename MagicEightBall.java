26b2cc98cf6379566bb69cfb094855c159bc742e
Alejandro Ojea <alojea@gmail.com>
2023-02-25 11:31:01 +0000

    Add files via upload

---
+++ MagicEightBall.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicEightBall {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> answers = generateRandomAnswers();
+		Random rand = new Random();
+		
+		while(true) {
+			System.out.print(answers.get(rand.nextInt(answers.size())) + '\n');
+			Scanner scanner = new Scanner(System.in);
+			scanner.nextLine();
+	  
+		}
+	}
+	
+	private static List<String> generateRandomAnswers() {
+		List<String> answers = List.of("I don't know","Weird","Excellent","I'm not sure","Everything is ok","All good");
+		return answers;
+	}
+}
